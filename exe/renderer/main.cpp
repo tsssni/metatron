f837c640b09c0ae293f53c2f515552811a48a82f
tsssni <dingyongyu2002@foxmail.com>
2025-04-07 20:01:25 +0800

    differential: fix infinite loop

--- exe/renderer/main.cpp
+++ exe/renderer/main.cpp
@@ -29,9 +29,9 @@ auto main() -> int {
 	color::Color_Space::initialize();
 
 	auto size = math::Vector<usize, 2>{1024uz};
-	auto spp = 128uz;
+	auto spp = 32uz;
 	auto blocks = 8uz;
-	auto kernels = std::max(1uz, usize(std::thread::hardware_concurrency()));
+	auto kernels = usize(std::thread::hardware_concurrency());
 
 	auto sensor = std::make_unique<photo::Sensor>(color::Color_Space::sRGB.get());
 	auto lens = std::make_unique<photo::Pinhole_Lens>(0.1f);
@@ -55,18 +55,17 @@ auto main() -> int {
 
 	auto sphere = shape::Sphere{};
 	auto diffuse = material::Diffuse_Material{
-		std::make_unique<material::Image_Texture<spectra::Stochastic_Spectrum>>(
-			image::Image::from_path("../Downloads/lines.png", false),
-			color::Color_Space::Spectrum_Type::albedo
+		std::make_unique<material::Constant_Texture<spectra::Stochastic_Spectrum>>(
+			std::make_unique<spectra::Constant_Spectrum>(0.1f)
 		),
 		std::make_unique<material::Constant_Texture<spectra::Stochastic_Spectrum>>(
-			std::make_unique<spectra::Constant_Spectrum>(0.0f)
+			std::make_unique<spectra::Constant_Spectrum>(0.5f)
 		),
 	};
 	auto homo_medium = media::Homogeneous_Medium{
-		std::make_unique<spectra::Constant_Spectrum>(0.1f),
+		std::make_unique<spectra::Constant_Spectrum>(0.0f),
 		std::make_unique<spectra::Constant_Spectrum>(0.5f),
-		std::make_unique<spectra::Constant_Spectrum>(0.01f),
+		std::make_unique<spectra::Constant_Spectrum>(0.0f),
 	};
 	auto bvh = accel::LBVH{{
 		{
--- src/render/monte-carlo/volume-path.cpp
+++ src/render/monte-carlo/volume-path.cpp
@@ -194,9 +194,9 @@ namespace metatron::mc {
 			
 			auto local_differential = lt ^ (rt ^ ctx.ray_differential);
 			auto tangent = shape::Plane{intr.p, intr.n};
-			OPTIONAL_OR_CONTINUE(d_intr, tangent(local_differential.r));
-			OPTIONAL_OR_CONTINUE(dx_intr, tangent(local_differential.rx));
-			OPTIONAL_OR_CONTINUE(dy_intr, tangent(local_differential.ry));
+			OPTIONAL_OR_BREAK(d_intr, tangent(local_differential.r));
+			OPTIONAL_OR_BREAK(dx_intr, tangent(local_differential.rx));
+			OPTIONAL_OR_BREAK(dy_intr, tangent(local_differential.ry));
 			
 			auto dpdx = dx_intr.p - d_intr.p;
 			auto dpdy = dy_intr.p - d_intr.p;
