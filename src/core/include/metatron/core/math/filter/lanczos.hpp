093233f1b1209f45e63c6ddf93f2178e862eae89
tsssni <dingyongyu2002@foxmail.com>
2025-04-30 00:16:26 +0800

    filter: add lanczos

---
+++ src/core/include/metatron/core/math/filter/lanczos.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <metatron/core/math/filter/filter.hpp>
+#include <metatron/core/math/distribution/piecewise.hpp>
+
+namespace metatron::math {
+	struct Lanczos_Filter final: Filter {
+		Lanczos_Filter(Vector<f32, 2> const& radius = {0.5f}, f32 tau = 3.f);
+
+		auto operator()(Vector<f32, 2> const& p) const -> f32;
+		auto sample(Vector<f32, 2> const& u) const -> std::optional<filter::Interaction>;
+
+	private:
+		Piecewise_Distribution<64, 64> distribution;
+		Vector<f32, 2> radius;
+		f32 tau;
+	};
+}
