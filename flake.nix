5a3cf0bb6bca91202cdf2f328f7922f42e6d15c4
tsssni <dingyongyu2002@foxmail.com>
2025-05-26 14:49:00 +0800

    geometry: add ext assimp

--- flake.nix
+++ flake.nix
@@ -1,37 +1,39 @@
 {
-	description = "metatron devenv";
+  description = "metatron devenv";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-	outputs = {
-		nixpkgs
-		, ...
-	}:
-	let
-		lib = nixpkgs.lib;
+  outputs =
+    {
+      nixpkgs,
+      ...
+    }:
+    let
+      lib = nixpkgs.lib;
 
-		systems = [
-			"aarch64-darwin"
-			"x86_64-linux"
-		];
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ];
 
-		mapSystems = f: systems
-			|> lib.map f
-			|> lib.mergeAttrsList;
+      mapSystems = f: systems |> lib.map f |> lib.mergeAttrsList;
 
-		packages = mapSystems (system:
-			let
-				pkgs = import nixpkgs {
-					inherit system;
-				};
-			in {
-				"${system}".default = pkgs.callPackage ./nix {};
-			}
-		);
+      packages = mapSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          "${system}".default = pkgs.callPackage ./nix { };
+        }
+      );
 
-	in { 
-		inherit packages;
-	};
+    in
+    {
+      inherit packages;
+    };
 }
