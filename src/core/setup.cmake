fbfcf7d09e2027f786cefe936b7ca0c0efb8fbe9
tsssni <dingyongyu2002@foxmail.com>
2025-07-10 00:42:38 +0800

    cmake: turn off pch so darwin compilation works

--- src/core/setup.cmake
+++ src/core/setup.cmake
@@ -1,3 +1,5 @@
 set(core-headers ${path}/include/metatron/core)
-target_precompile_headers(${target} ${access} ${core-headers}/prelude/prelude.hpp)
+target_compile_options(${target} ${access}
+	-include ${core-headers}/prelude/prelude.hpp
+)
 list(APPEND metatron-deps mimalloc proxy)
