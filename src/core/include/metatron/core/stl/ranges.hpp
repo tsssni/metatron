aaf6a7ec307272e7e4276021608f1fd5a0af0ab5
tsssni <dingyongyu2002@foxmail.com>
2025-09-05 00:58:40 +0800

    ranges: fix wrong reference on apple clang

--- src/core/include/metatron/core/stl/ranges.hpp
+++ src/core/include/metatron/core/stl/ranges.hpp
@@ -3,9 +3,9 @@
 
 namespace mtt::stl::ranges::views {
 #ifdef __APPLE__
-	auto inline constexpr cartesian_product = [](auto a, auto b) {
-		return a | std::views::transform([&](auto i) {
-			return b | std::views::transform([i](auto j) {
+	auto inline constexpr cartesian_product = [](auto&& a, auto&& b) {
+		return a | std::views::transform([b = std::forward<decltype(b)>(b)](auto&& i) mutable {
+			return b | std::views::transform([i = std::forward<decltype(i)>(i)](auto&& j) {
 				return std::make_pair(i, j);
 			});
 		}) | std::views::join;
