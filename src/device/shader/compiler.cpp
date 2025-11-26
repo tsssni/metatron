d7477bfd4fc2bb874f7af1e0a74590e059751f51
tsssni <dingyongyu2002@foxmail.com>
2025-11-27 00:18:37 +0800

    compiler: support basic compilation cache

--- src/device/shader/compiler.cpp
+++ src/device/shader/compiler.cpp
@@ -1,64 +1,88 @@
 #include <metatron/device/shader/compiler.hpp>
+#include <metatron/core/stl/filesystem.hpp>
 #include <metatron/core/stl/print.hpp>
 #include <slang-com-ptr.h>
 #include <slang.h>
 #include <filesystem>
 #include <fstream>
+#include <cstring>
 
 namespace mtt::shader {
+    #ifdef __linux__
+        auto constexpr format = SLANG_SPIRV;
+        auto constexpr ext = std::string_view{".spirv"};
+    #elif __APPLE__
+        auto constexpr format = SLANG_METAL_LIB;
+        auto constexpr ext = std::string_view{".metallib"};
+    #endif
+    auto constexpr kernel = std::string_view{".kernel.slang"};
+
     struct Compiler::Impl final {
         std::filesystem::path dir;
         std::filesystem::path out;
         Slang::ComPtr<slang::IGlobalSession> global_session;
+        Slang::ComPtr<slang::ISession> session;
 
-        Impl() noexcept {
-            slang::createGlobalSession(global_session.writeRef());
-        }
+        Impl() noexcept {}
 
-        auto compile(cref<std::filesystem::path> src) noexcept -> void {
-            #ifdef __linux__
-                auto constexpr format = SLANG_SPIRV;
-                auto constexpr ext = std::string_view{".spirv"};
-            #elif __APPLE__
-                auto constexpr format = SLANG_METAL_LIB;
-                auto constexpr ext = std::string_view{".metallib"};
-            #endif
+        auto compile(mut<slang::IModule> module, i32 idx, std::filesystem::path src) noexcept -> void {
+            auto diagnostic = Slang::ComPtr<slang::IBlob>{};
+            auto guard = [](SlangResult result) {
+                if (SLANG_FAILED(result)) {
+                    std::println("slang compilation failed with {}", result);
+                    std::abort();
+                }
+            };
+            auto print = [&diagnostic, &guard](SlangResult result) {
+                if (diagnostic && diagnostic->getBufferSize() > 0)
+                    std::println("{}", view<char>(diagnostic->getBufferPointer()));
+                guard(result);
+            };
 
-            auto paths = std::to_array({dir.c_str()});
-            auto targets = std::array<slang::TargetDesc, 1>{};
-            auto& target = targets.front();
-            target.format = format;
-            target.forceGLSLScalarBufferLayout = true;
-
-            auto session = Slang::ComPtr<slang::ISession>{};
-            global_session->createSession({
-                .targets = targets.data(),
-                .targetCount = targets.size(),
-                .defaultMatrixLayoutMode = SLANG_MATRIX_LAYOUT_ROW_MAJOR,
-                .searchPaths = paths.data(),
-                .searchPathCount = paths.size(),
-            }, session.writeRef());
-
-            auto module = session->loadModule(src.stem().c_str());
             auto entry = Slang::ComPtr<slang::IEntryPoint>{};
-            module->findEntryPointByName("main", entry.writeRef());
+            auto hash = Slang::ComPtr<slang::IBlob>{};
+            guard(module->getDefinedEntryPoint(idx, entry.writeRef()));
+            module->getEntryPointHash(idx, 0, hash.writeRef());
+            if (!hash || hash->getBufferSize() == 0) {
+                std::println(
+                    "slang failed to generate hash for {} {}",
+                    src.c_str(), entry->getFunctionReflection()->getName()
+                );
+                std::abort();
+            }
 
-            auto program = Slang::ComPtr<slang::IComponentType>{};
-            auto components = std::to_array<mut<slang::IComponentType>>({module, entry});
-            session->createCompositeComponentType(components.data(), 2, program.writeRef());
+            auto file = src.parent_path() / std::filesystem::path{module->getName()}.stem().stem()
+            .concat("-" + std::string{entry->getFunctionReflection()->getName()}).concat(ext);
+            auto cache = (stl::filesystem::instance().cache / file).concat(".mttcache");
+            std::filesystem::create_directories(cache.parent_path());
+
+            if ([&] {
+                if (!std::filesystem::exists(cache)) return false;
+                auto size = std::filesystem::file_size(cache);
+                if (size != hash->getBufferSize()) return false;
+                auto stream = std::ifstream{cache, std::ios::binary};
+                auto buffer = std::vector<char>(size);
+                stream.read(buffer.data(), buffer.size());
+                return std::memcmp(buffer.data(), hash->getBufferPointer(), size) == 0;
+            }()) return;
+            auto version = std::ofstream{cache, std::ios::binary};
+            version.write(view<char>(hash->getBufferPointer()), hash->getBufferSize());
+
+            auto code = Slang::ComPtr<slang::IComponentType>{};
+            auto unit = std::to_array<mut<slang::IComponentType>>({module, entry});
+            print(session->createCompositeComponentType(
+                unit.data(), unit.size(),
+                code.writeRef(), diagnostic.writeRef())
+            );
 
             auto linked = Slang::ComPtr<slang::IComponentType>{};
-            auto diagnostic = Slang::ComPtr<slang::IBlob>{};
             auto kernel = Slang::ComPtr<slang::IBlob>{};
-            program->link(linked.writeRef(), diagnostic.writeRef());
-            linked->getEntryPointCode(0, 0, kernel.writeRef(), diagnostic.writeRef());
+            print(code->link(linked.writeRef(), diagnostic.writeRef()));
+            print(linked->getEntryPointCode(0, 0, kernel.writeRef(), diagnostic.writeRef()));
 
-            if (diagnostic && diagnostic->getBufferSize() > 0)
-                std::println("{}", view<char>(diagnostic->getBufferPointer()));
-
-            auto dst = out / src.stem().stem().concat(ext);
-            auto file = std::ofstream{dst, std::ios::binary};
-            file.write(view<char>(kernel->getBufferPointer()), kernel->getBufferSize());
+            auto compiled = std::ofstream{out / file, std::ios::binary};
+            std::filesystem::create_directories((out / file).parent_path());
+            compiled.write(view<char>(kernel->getBufferPointer()), kernel->getBufferSize());
         }
 
         auto build(
@@ -67,11 +91,35 @@ namespace mtt::shader {
         ) noexcept -> void {
             this->dir = dir;
             this->out = out;
-            for (auto& entry: std::filesystem::recursive_directory_iterator(this->dir)) {
-                auto constexpr kernel_extension = std::string_view{".kernel.slang"};
-                if (entry.path().string().ends_with(kernel_extension))
-                    compile(entry.path());
-            }
+
+            auto targets = std::array<slang::TargetDesc, 1>{};
+            targets.front().format = format;
+            targets.front().forceGLSLScalarBufferLayout = true;
+            auto paths = std::to_array({dir.c_str()});
+            auto options = std::to_array<slang::CompilerOptionEntry>({
+                {
+                    .name = slang::CompilerOptionName::ForceCLayout,
+                    .value = {.intValue0 = 1}
+                }
+            });
+
+            slang::createGlobalSession(global_session.writeRef());
+            global_session->createSession({
+                .targets = targets.data(),
+                .targetCount = targets.size(),
+                .defaultMatrixLayoutMode = SLANG_MATRIX_LAYOUT_ROW_MAJOR,
+                .searchPaths = paths.data(),
+                .searchPathCount = paths.size(),
+                .compilerOptionEntries = options.data(),
+                .compilerOptionEntryCount = options.size(),
+            }, session.writeRef());
+
+            for (auto& src: std::filesystem::recursive_directory_iterator(this->dir))
+                if (src.path().string().ends_with(kernel)) {
+                    auto module = session->loadModule(src.path().c_str());
+                    for (auto i = 0; i < module->getDefinedEntryPointCount(); ++i)
+                        compile(module, i, src);
+                }
         }
     };
 
